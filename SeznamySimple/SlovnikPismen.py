499c43133d1050e73885e4fc745300060155b846
MaJo4Long <113591537+MaJo4Long@users.noreply.github.com>
2022-11-04 00:00:24 +0100

    Add files via upload

---
+++ SeznamySimple/SlovnikPismen.py
@@ -0,0 +1,15 @@
+from string import ascii_letters
+
+
+def counting_letters(text):
+    dictionary = {}
+    for i in text:
+        if i in ascii_letters:
+            dictionary[i] = text.count(i)
+    return dictionary
+
+
+textconvert = input("Write something to count your words: ")
+
+text = textconvert.lower()
+print(counting_letters(text))
